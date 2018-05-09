a9e9f1c8164b617464bb003ea8c808fcfa5737f4
Joanna Buchała <joanna.b2@op.pl>
2018-05-10 00:34:15 +0200

    alt

--- components/Contact.js
+++ components/Contact.js
@@ -8,7 +8,8 @@ var Contact = React.createClass({
       React.createElement('div', {className: 'contactItem'},
         React.createElement('img', {
           className: 'contactImage',
-          src: 'https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/256x256/plain/user.png'
+          src: 'https://d1nhio0ox7pgb.cloudfront.net/_img/o_collection_png/green_dark_grey/256x256/plain/user.png',
+          alt: 'person'
         }),
         React.createElement('p', {className: 'contactLabel'}, 'Name: ' + this.props.item.firstName),
         React.createElement('p', {className: 'contactLabel'}, 'Last Name: ' + this.props.item.lastName),
